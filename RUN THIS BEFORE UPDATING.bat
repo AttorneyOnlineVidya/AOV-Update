bc931f46ceafd753e8f44286a29b27d895cfdb5c
NamuTelling <turtleswastaken@gmail.com>
2018-03-13 20:44:59 -0500

    Did the Butz Suggestion + Updated Bat

--- RUN THIS BEFORE UPDATING.bat
+++ RUN THIS BEFORE UPDATING.bat
@@ -1,2 +1,54 @@
 ren base\sounds\music\"[MGSR] Pursuit.mp3" "[MGR] Pursuit.mp3"
 ren base\sounds\music\"[DP] Confess the Truth.mp3" "[DP] York and Zach.mp3"
+
+del base\characters\Butz\(a)samuraicry.gif
+del base\characters\Butz\(b)samuraicry.gif
+del base\characters\Butz\(a)samuraiscratch.gif
+del base\characters\Butz\(b)samuraiscratch.gif
+del base\characters\Butz\(a)samuraithink.gif
+del base\characters\Butz\(b)samuraithink.gif
+del base\characters\Butz\(a)samuraithumb.gif
+del base\characters\Butz\(b)samuraithumb.gif
+del base\characters\Butz\(a)steelsamurai-cry.gif
+del base\characters\Butz\(b)steelsamurai-cry.gif
+del base\characters\Butz\(a)steelsamurai-mask.gif
+del base\characters\Butz\(b)steelsamurai-mask.gif
+del base\characters\Butz\(a)steelsamurai-nervous.gif
+del base\characters\Butz\(b)steelsamurai-nervous.gif
+del base\characters\Butz\(a)steelsamurai-normal.gif
+del base\characters\Butz\(b)steelsamurai-normal.gif
+del base\characters\Butz\(a)steelsamurai-scratch.gif
+del base\characters\Butz\(b)steelsamurai-scratch.gif
+del base\characters\Butz\(a)steelsamurai-thinking.gif
+del base\characters\Butz\(b)steelsamurai-thinking.gif
+del base\characters\Butz\(a)steelsamurai-thumb.gif
+del base\characters\Butz\(b)steelsamurai-thumb.gif
+del base\characters\Butz\(a)steelsamurai-yay.gif
+del base\characters\Butz\(b)steelsamurai-yay.gif
+del base\characters\Butz\steelsamuraiLAUGH.gif
+
+del base\characters\Butz\emotions\button27_off.png
+del base\characters\Butz\emotions\button28_off.png
+del base\characters\Butz\emotions\button29_off.png
+del base\characters\Butz\emotions\button30_off.png
+del base\characters\Butz\emotions\button31_off.png
+del base\characters\Butz\emotions\button32_off.png
+del base\characters\Butz\emotions\button33_off.png
+del base\characters\Butz\emotions\button34_off.png
+del base\characters\Butz\emotions\button35_off.png
+del base\characters\Butz\emotions\button36_off.png
+del base\characters\Butz\emotions\button37_off.png
+del base\characters\Butz\emotions\button38_off.png
+
+del base\characters\Butz\emotions\button27_on.png
+del base\characters\Butz\emotions\button28_on.png
+del base\characters\Butz\emotions\button29_on.png
+del base\characters\Butz\emotions\button30_on.png
+del base\characters\Butz\emotions\button31_on.png
+del base\characters\Butz\emotions\button32_on.png
+del base\characters\Butz\emotions\button33_on.png
+del base\characters\Butz\emotions\button34_on.png
+del base\characters\Butz\emotions\button35_on.png
+del base\characters\Butz\emotions\button36_on.png
+del base\characters\Butz\emotions\button37_on.png
+del base\characters\Butz\emotions\button38_on.png
