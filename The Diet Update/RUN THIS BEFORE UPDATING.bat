762852503b1fca61c696252788cd10fdd650f257
NamuTelling <turtleswastaken@gmail.com>
2018-06-02 14:55:01 -0500

    bye bye AthenaW; AthenaSOJ and AthenaDA

--- The Diet Update/RUN THIS BEFORE UPDATING.bat
+++ The Diet Update/RUN THIS BEFORE UPDATING.bat
@@ -1,2 +1,6 @@
 del "base\characters\Filch\custom.gif"
 del "base\characters\Filch\custom.wav"
+
+rd /S /Q "base\characters\AthenaDA"
+rd /S /Q "base\characters\AthenaSOJ"
+rd /S /Q "base\characters\AthenaW"
