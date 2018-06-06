9b2543699d3f5157b2084b979bb8c7794d4344df
NamuTelling <turtleswastaken@gmail.com>
2018-06-06 18:45:34 -0500

    Delet PhoenixDD's folder

--- The Steamed Update/RUN THIS BEFORE UPDATING.bat
+++ The Steamed Update/RUN THIS BEFORE UPDATING.bat
@@ -6,4 +6,6 @@ rd /S /Q "base\characters\AthenaSOJ"
 rd /S /Q "base\characters\AthenaW"
 
 move "base\characters\Judge's Bro ET" "base\characters\Judge's Bro"
-move "base\characters\Judge ET" "base\characters\Judge" 
+move "base\characters\Judge ET" "base\characters\Judge"
+
+rd /S /Q "base\characters\PhoenixDD"
