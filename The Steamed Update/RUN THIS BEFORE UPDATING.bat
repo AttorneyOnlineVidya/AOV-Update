c9cf418744b9879a652cc276a597d6c17f8c3a38
NamuTelling <turtleswastaken@gmail.com>
2018-06-07 18:23:54 -0500

    Renamed [DGS2] Confess the Truth to [DGS2] The Gate to the Truth and moved it to the Reminiscence section.

--- The Steamed Update/RUN THIS BEFORE UPDATING.bat
+++ The Steamed Update/RUN THIS BEFORE UPDATING.bat
@@ -9,3 +9,5 @@ move "base\characters\Judge's Bro ET" "base\characters\Judge's Bro"
 move "base\characters\Judge ET" "base\characters\Judge"
 
 rd /S /Q "base\characters\PhoenixDD"
+
+ren "base\sounds\music\[DGS2] Confess the Truth.mp3" "[DGS2] The Gate to the Truth.mp3"
