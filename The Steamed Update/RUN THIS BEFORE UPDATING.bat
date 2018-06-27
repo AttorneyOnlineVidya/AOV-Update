09b69ffa358a1d4705f727078bd3be0839f6ea51
NamuTelling <turtleswastaken@gmail.com>
2018-06-27 17:31:53 -0500

    De Killer has been separated from the AAI folder!

--- The Steamed Update/RUN THIS BEFORE UPDATING.bat
+++ The Steamed Update/RUN THIS BEFORE UPDATING.bat
@@ -26,3 +26,54 @@ rd /S /Q "base\characters\Payne TT"
 
 rd /S /Q "base\characters\Miles Wit"
 rd /S /Q "base\characters\Godot HLD"
+
+del "base\characters\de Killer AAI\(a)killer-bellboy.gif"
+del "base\characters\de Killer AAI\(b)killer-bellboy.gif"
+del "base\characters\de Killer AAI\(a)killer-damage.gif"
+del "base\characters\de Killer AAI\(b)killer-damage.gif"
+del "base\characters\de Killer AAI\(a)killer-doe.gif"
+del "base\characters\de Killer AAI\(b)killer-doe.gif"
+del "base\characters\de Killer AAI\(a)killer-normal.gif"
+del "base\characters\de Killer AAI\(b)killer-normal.gif"
+del "base\characters\de Killer AAI\(a)killer-red.gif"
+del "base\characters\de Killer AAI\(b)killer-red.gif"
+del "base\characters\de Killer AAI\(a)killer-steaming.gif"
+del "base\characters\de Killer AAI\(b)killer-steaming.gif"
+del "base\characters\de Killer AAI\(a)killer-sweating.gif"
+del "base\characters\de Killer AAI\(b)killer-sweating.gif"
+del "base\characters\de Killer AAI\killer-bellboy(a).gif"
+del "base\characters\de Killer AAI\killer-bellboy(b).gif"
+del "base\characters\de Killer AAI\killer-bellboy.gif"
+del "base\characters\de Killer AAI\killer-damage.gif"
+del "base\characters\de Killer AAI\killer-doe(a).gif"
+del "base\characters\de Killer AAI\killer-doe(b).gif"
+del "base\characters\de Killer AAI\killer-doe.gif"
+del "base\characters\de Killer AAI\killer-normal.gif"
+del "base\characters\de Killer AAI\killer-red.gif"
+del "base\characters\de Killer AAI\killer-steaming.gif"
+del "base\characters\de Killer AAI\killer-sweating.gif"
+
+del "base\characters\de Killer AAI\emotions\button7_off.png"
+del "base\characters\de Killer AAI\emotions\button7_on.png"
+
+del "base\characters\de Killer AAI\emotions\button8_off.png"
+del "base\characters\de Killer AAI\emotions\button8_on.png"
+
+del "base\characters\de Killer AAI\emotions\button9_off.png"
+del "base\characters\de Killer AAI\emotions\button9_on.png"
+
+del "base\characters\de Killer AAI\emotions\button10_off.png"
+del "base\characters\de Killer AAI\emotions\button10_on.png"
+
+del "base\characters\de Killer AAI\emotions\button11_off.png"
+del "base\characters\de Killer AAI\emotions\button11_on.png"
+
+del "base\characters\de Killer AAI\emotions\button12_off.png"
+del "base\characters\de Killer AAI\emotions\button12_on.png"
+
+del "base\characters\de Killer AAI\emotions\button13_off.png"
+del "base\characters\de Killer AAI\emotions\button13_on.png"
+
+del "base\characters\de Killer AAI\emotions\button14_off.png"
+del "base\characters\de Killer AAI\emotions\button14_on.png"
+
