7a802e37c73fae5040e292722140a7014a7e3d7c
Ryebred <40445041+Ryebred@users.noreply.github.com>
2018-07-02 15:41:50 +0100

    Removed Blaise's old unused buttons
    15 and 16 were unused.

--- Our Update/RUN THIS BEFORE UPDATING.bat
+++ Our Update/RUN THIS BEFORE UPDATING.bat
@@ -1,1 +1,5 @@
 rd /S /Q "base\characters\Klavier Wit"
+del "base\characters\Blaise Debeste\emotions\button15_on.png"
+del "base\characters\Blaise Debeste\emotions\button15_off.png"
+del "base\characters\Blaise Debeste\emotions\button16_on.png"
+del "base\characters\Blaise Debeste\emotions\button16_off.png"
