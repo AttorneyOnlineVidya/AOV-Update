54fc81b27da8a5f85f9b16f30844c733f2c86d45
Ryebred <40445041+Ryebred@users.noreply.github.com>
2018-07-02 16:25:45 +0100

    Removed useless pre-animations from de Killer
    killer-bellboy, killer-doe, killer-normal, killer-red, killer-steaming and killer-sweating are unnecessary.

--- Our Update/RUN THIS BEFORE UPDATING.bat
+++ Our Update/RUN THIS BEFORE UPDATING.bat
@@ -19,3 +19,10 @@ rd /S /Q "base\characters\AthenaCykesSOJ\emotions (matrix)"
 rd /S /Q "base\characters\AthenaCykesSOJ\emotions(ultimate)"
 
 rd /S /Q "base\characters\AthenaH\emotions\Old"
+
+del "base\characters\de Killer\killer-bellboy.gif"
+del "base\characters\de Killer\killer-doe.gif"
+del "base\characters\de Killer\killer-normal.gif"
+del "base\characters\de Killer\killer-red.gif"
+del "base\characters\de Killer\killer-steaming.gif"
+del "base\characters\de Killer\killer-sweating.gif"
