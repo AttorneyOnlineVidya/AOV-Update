1834372dc17f26826e18cf3ebb98fc37748c5adc
NamuTelling <turtleswastaken@gmail.com>
2018-07-02 10:00:36 -0500

    Order is a good thing

--- Our Update/RUN THIS BEFORE UPDATING.bat
+++ Our Update/RUN THIS BEFORE UPDATING.bat
@@ -1,8 +1,10 @@
 rd /S /Q "base\characters\Klavier Wit"
+
 del "base\characters\Blaise Debeste\emotions\button15_on.png"
 del "base\characters\Blaise Debeste\emotions\button15_off.png"
 del "base\characters\Blaise Debeste\emotions\button16_on.png"
 del "base\characters\Blaise Debeste\emotions\button16_off.png"
+
 del "base\characters\Cammy\emotions\buttondumbshit_off.png"
 del "base\characters\Cammy\emotions\buttondumbshit_on.png"
 del "base\characters\Cammy\emotions\buttonsweat_on.png"
