9635f80417b4c5de01d0c39b543c8b18ee77df0b
Jumbowl <collinxchu@gmail.com>
2018-10-14 23:45:00 -0300

    Oh hey it's stupid easy to fix Elise
    Fixes elise's button names with the power of blast processing

--- The Nightmare Update/RUN THIS BEFORE UPDATING.bat
+++ The Nightmare Update/RUN THIS BEFORE UPDATING.bat
@@ -65,5 +65,10 @@ del "base\characters\Phoenix Furio\think.gif"
 del "base\characters\Phoenix Furio\urk.gif"
 del "base\characters\Phoenix Furio\yes.gif"
 
+ren base\characters\Elise\emotions\button8_off.png button1_off.png
+ren base\characters\Elise\emotions\button8_on.png button1_on.png
+ren base\characters\Elise\emotions\button9_off.png button2_off.png
+ren base\characters\Elise\emotions\button9_on.png button2_on.png
+
 echo "Nightmare Update Completed"
 pause
