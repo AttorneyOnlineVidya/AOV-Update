01e3d8393242fde152c41772bcde9814fcdcc48e
Jumbowl <collinxchu@gmail.com>
2018-10-28 01:38:54 -0300

    Elise Button Fix 2: Synaptic Boozagoo (#11)
    I've run out of jokes but hey here's the same batch lines as last time.

--- RUN THIS BEFORE UPDATING.bat
+++ RUN THIS BEFORE UPDATING.bat
@@ -1,4 +1,7 @@
-
+ren base\characters\Elise\emotions\button8_off.png button1_off.png
+ren base\characters\Elise\emotions\button8_on.png button1_on.png
+ren base\characters\Elise\emotions\button9_off.png button2_off.png
+ren base\characters\Elise\emotions\button9_on.png button2_on.png
 
 echo "completed"
 pause
