390e1ddcb203d1f5de3c869068a3767d1538dacd
Richard Ravage <44098096+Dickwrath@users.noreply.github.com>
2018-10-29 23:35:49 +0000

    Corrections and junk
    Heita's theme is now from the correct game, the .bat got updated and the anime.mp3 is added.

--- RUN THIS BEFORE UPDATING.bat
+++ RUN THIS BEFORE UPDATING.bat
@@ -3,9 +3,12 @@ ren base\characters\Elise\emotions\button8_on.png button1_on.png
 ren base\characters\Elise\emotions\button9_off.png button2_off.png
 ren base\characters\Elise\emotions\button9_on.png button2_on.png
 
-del base\characters\Trucy
-del base\characters\Means
-del base\characters\Payne DGS
+ren "base\sounds\music\[DGS] Mamemomi Heita - Justice from Reporter Mamemomi.mp3" "[DGS2] Mamemomi Heita - Justice from Reporter Mamemomi.mp3"
+ren "base\sounds\music\test.mp3" "[MOD] abuse.mp3"
+
+rd /S /Q base\characters\Trucy
+rd /S /Q base\characters\Means
+rd /S /Q base\characters\Payne DGS
 
 echo "completed"
-pause
+exit
