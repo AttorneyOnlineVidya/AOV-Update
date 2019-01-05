e04d7101916a9411fbea07a795ae5c77718486ca
Richard Ravage <44098096+Dickwrath@users.noreply.github.com>
2019-01-05 18:01:52 +0000

    Unbotched the bigg botcch
    Now with the actual files

--- And A Happy New Update/RUN THIS BEFORE UPDATING.bat
+++ And A Happy New Update/RUN THIS BEFORE UPDATING.bat
@@ -1,26 +1,4 @@
 
-del "base\characters\Margaret\[PRE]-damage.gif"
-del "base\characters\Margaret\[PRE]-Burst.gif"
-del "base\characters\Margaret\[PRE]-enough.gif"
-del "base\characters\Margaret\[PRE]-intro.gif"
-del "base\characters\Margaret\[PRE]-objection.gif"
-del "base\characters\Margaret\[PRE]-oof.gif"
-del "base\characters\Margaret\[PRE]-Peace.gif"
-del "base\characters\Margaret\[PRE]-present.gif"
-del "base\characters\Margaret\[PRE]-shock.gif"
-del "base\characters\Margaret\[PRE]-show.gif"
-del "base\characters\Margaret\[PRE]-slam.gif"
-del "base\characters\Margaret\[PRE]-slamX2.gif"
-del "base\characters\Margaret\[PRE]-slip.gif"
-del "base\characters\Margaret\[PRE]-swipe.gif"
-
-del "base\sounds\music\[AOV] anime.mp3"
-del "base\sounds\music\[DGS] Detention Center.mp3"
-del "base\sounds\music\[DGS] Joint Reasoning (Type B-2).mp3"
-del "base\sounds\music\[DGS] Reasoning Battle.mp3"
-del "base\sounds\music\[DGS] Reasoning Battle (Allegro).mp3"
-del "base\sounds\music\[DGS] The Musings of Sherlock Holmes.mp3"
-
 rd /S /Q base\characters\Justine CourtneyPro
 
 echo "completed"
