6b6a452f3a70f766e2e7183d1dfe94fb05756388
Richard Ravage <44098096+Dickwrath@users.noreply.github.com>
2019-04-28 00:47:45 +0100

    Added this month's FUCKING update
    It gives us FUCK ALL aside from a few new songs and rerips. The only new character is A FUCKING DOG.

--- The Mature Update/RUN THIS BEFORE UPDATING.bat
+++
@@ -1,9 +0,0 @@
-
-del "base\sounds\general\drv3-oumasnore.wav"
-del "base\sounds\general\drv3-oumacry.wav"
-del "base\sounds\general\drv3-oumalaugh"
-del "base\sounds\general\drv3-usodayo.wav"
-del "base\sounds\general\drv3-nishishi.wav"
-
-echo "completed"
-exit
