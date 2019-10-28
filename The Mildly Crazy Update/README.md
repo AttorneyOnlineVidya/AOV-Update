629feddcc25da9fe877f045cda2941912e1b58a7
AnUnknownQuantity <54252389+AnUnknownQuantity@users.noreply.github.com>
2019-10-28 14:51:01 -0500

    Preliminary Pass of November Update

---
+++ The Mildly Crazy Update/README.md
@@ -0,0 +1,14 @@
+# [THE MILDLY CRAZY UPDATE]
+
+Content additions:
+   * Eve Belduke has been added to the roster
+
+Botch fixes:
+   * Fixed some of Chelmey's buttons having the wrong coloring scheme
+   * Fixed Ridelle's folder for those using the Full Download/Launcher version
+   * EmaAJ's snackoo-eating sfx have been cleaned up
+
+Content changes:
+   * Dante has a new and improved folder
+   * Updated the anime.opus
+
---
+++ The Mildly Crazy Update/RUN THIS BEFORE UPDATING.bat
@@ -0,0 +1,11 @@
+del "base\sounds\general\aj-ema-munch.*" /s /q
+del "base\characters\Dante" /s /q
+del "base\characters\Ridelle" /s /q
+del "base\characters\Chelmey\Emotions\button11_off.*" /s /q
+del "base\characters\Chelmey\Emotions\button11_on.*" /s /q
+del "base\characters\Chelmey\Emotions\button12_off.*" /s /q
+del "base\characters\Chelmey\Emotions\button12_on.*" /s/q
+del "base\characters\EmaAJ\(a)nackoo.*" /s /q
+
+echo "completed"
+exit
