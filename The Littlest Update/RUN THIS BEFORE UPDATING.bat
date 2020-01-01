5160c3c0ec724c01d3cad00182b590e61e847edf
AnUnknownQuantity <54252389+AnUnknownQuantity@users.noreply.github.com>
2020-01-01 16:56:29 -0600

    Cleaning up of old updates!

--- The Littlest Update/RUN THIS BEFORE UPDATING.bat
+++
@@ -1,9 +0,0 @@
-del "base\sounds\music\[BB] Confess the Truth.*" /s
-del "base\sounds\music\[BB] Dangerous People.*" /s
-del "base\sounds\music\[BB] Malicious People.*" /s
-del "base\characters\Fulbright\bobby button.png" /s /q
-del "base\characters\Fulbright\bobby button.pdn" /s /q
-
-
-echo "completed"
-exit
