59e5cad6b9a0cdc02241ac10c0ccbe637550c4e0
AnUnknownQuantity <54252389+AnUnknownQuantity@users.noreply.github.com>
2020-06-28 12:49:30 -0500

    Preliminary Pass of July Update!

--- The Sick and Spreading Update/RUN THIS BEFORE UPDATING.bat
+++
@@ -1,7 +0,0 @@
-rmdir "base\characters\Chiaki" /s /q
-rmdir "base\characters\Ponco" /s /q
-ren "base\sounds\music\[DGS2] Dr. Dobinbough - Science Boy.*" "[DGS2] Dr. Benjamin Dobinbough - Science Boy.*"
-del "base\sounds\music\[AOV] anime.opus" /s /q
-
-echo "completed"
-exit
---
+++ The Stars and Stripes Update/RUN THIS BEFORE UPDATING.bat
@@ -0,0 +1,23 @@
+rmdir "base\characters\Marshall" /s /q
+rmdir "base\background\AACourtAnime" /s /q
+del "base\characters\Sahwit\breakdown1.*" /s /q
+del "base\characters\Sahwit\breakdown2.*" /s /q
+del "base\characters\Sahwit\char - Kopie.ini" /s /q
+del "base\characters\Sirhan Dogen\enterkuroold.*" /s /q
+del "base\characters\Patricia\damage.*" /s /q
+del "base\characters\Patricia\breakdown.*" /s /q
+del "base\characters\Vasquez\dee-surprised(a).*" /s /q
+del "base\characters\Beppo\emotions" /s /q
+del "base\characters\Luke\emotions" /s /q
+del "base\characters\Yuujin Mikotoba Suit\emotions" /s /q
+del "base\characters\Sirhan Dogen\emotions" /s /q
+del "base\characters\Sahwit\emotions" /s /q
+del "base\characters\Vasquez\emotions" /s /q
+del "base\characters\Bowl\emotions" /s /q
+del "base\characters\Marie\(a)Flustered.apng" /s /q
+del "base\characters\Marie\(b)Flustered.apng" /s /q
+del "base\sounds\music\[AOV] anime.opus" /s /q
+
+
+echo "completed"
+exit
