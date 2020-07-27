9ea3c8c38497afecd937ae7e2d8acf5e2a8a6c0b
AnUnknownQuantity <54252389+AnUnknownQuantity@users.noreply.github.com>
2020-07-27 09:18:55 -0500

    Preliminary pass of August Update!

---
+++ The Record-Spinning and Revamping Update/RUN THIS BEFORE UPDATING.bat
@@ -0,0 +1,68 @@
+del "base\sounds\music\[AOV] anime.opus" /s /q
+del "base\sounds\music\[JJ] Logic and Stands.opus" /s /q
+ren "base\sounds\music\[O] Suspense.*" "[O] Objection.*" /s /q
+rmdir "base\characters\MilesDD AAI\emotions" /s /q
+del "base\characters\MilesDD AAI\Pre-Bow.*" /s /q
+del "base\characters\MilesDD AAI\Pre-Objection.*" /s /q
+del "base\characters\MilesDD AAI\Pre-Shock.*" /s /q
+rmdir "base\characters\Matilda\emotions" /s /q
+rmdir "base\characters\Matilda Headset\emotions" /s /q
+rmdir "base\characters\0 Master Anonymous\emotions" /s /q
+rmdir "base\characters\1 Anonymous\emotions" /s /q
+rmdir "base\characters\2 Anonymous\emotions" /s /q
+rmdir "base\characters\3 Anonymous\emotions" /s /q
+rmdir "base\characters\4 Anonymous\emotions" /s /q
+del "base\characters\Penny AAI\emotions\button4_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button4_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button5_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button5_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button6_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button6_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button7_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button7_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button8_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button8_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button9_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button9_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button10_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button10_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button11_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button11_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button12_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button12_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button13_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button13_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button14_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button14_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button15_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button15_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button16_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button16_on.*" /s /q
+del "base\characters\Penny AAI\emotions\button17_off.*" /s /q
+del "base\characters\Penny AAI\emotions\button17_on.*" /s /q
+rmdir "base\characters\LanaP\emotions" /s /q
+del "base\characters\Ron\(a)ron-disappointed.*" /s /q
+del "base\characters\Ron\(b)ron-disappointed.*" /s /q
+del "base\characters\Ron\(a)ron-smug.*" /s /q
+del "base\characters\Ron\(b)ron-smug.*" /s /q
+del "base\characters\Ron\(a)ron-normal.*" /s /q
+del "base\characters\Ron\(b)ron-normal.*" /s /q
+del "base\characters\Ron\(a)ron-sweating.*" /s /q
+del "base\characters\Ron\(b)ron-sweating.*" /s /q
+del "base\characters\Ron\(a)ron-thinking.*" /s /q
+del "base\characters\Ron\(b)ron-thinking.*" /s /q
+del "base\characters\Ron\(a)ron-emphatic.*" /s /q
+del "base\characters\Ron\(b)ron-emphatic.*" /s /q
+del "base\characters\Ron\(a)ron-shouting.*" /s /q
+del "base\characters\Ron\(b)ron-shouting.*" /s /q
+rmdir "base\characters\Ron\emotions" /s /q
+rmdir "base\characters\Sonohigurashi\emotions" /s /q
+del "base\characters\Seven\sevencutin.*" /s /q
+del "base\characters\Seven\sevencutinhp.*" /s /q
+del "base\sounds\music\[DR] Objection!.*" /s /q
+del "base\sounds\music\[P5] Logic of the Heart.*" /s /q
+del "base\sounds\music\[Misc] It's Always Sunny.*" /s /q
+del "base\sounds\music\[P5] Prelude.*" /s /q
+
+echo "completed"
+exit
