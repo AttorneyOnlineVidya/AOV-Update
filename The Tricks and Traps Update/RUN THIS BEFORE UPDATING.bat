352cce80bc211837c83571bab77e5cb506b570b8
Jumbowl <collinxchu@gmail.com>
2020-10-02 22:51:23 -0300

    Clean up #2

--- The Tricks and Traps Update/RUN THIS BEFORE UPDATING.bat
+++ The Tricks and Traps Update/RUN THIS BEFORE UPDATING.bat
@@ -155,7 +155,7 @@ ren "base\characters\LeTouse" Romein
 ren "base\characters\Long Lang" Shi-Long
 ren "base\characters\Lotta AAI" Lotta_AAI
 ren "base\characters\Lucy Baker" Lucy
-ren "base\characters\Luke Adult" Luke_Adult
+ren "base\characters\Luke Adult" Luke_Triton_Adult
 ren "base\characters\Machi Tobaye" Machi
 ren "base\characters\Maggey AAI" Maggey_AAI
 ren "base\characters\Makepeace" Diane
