1ac06b2adc667ac7f0340e1afafcd8aaafb7add6
AnUnknownQuantity <54252389+AnUnknownQuantity@users.noreply.github.com>
2021-08-31 10:29:21 -0500

    Second Pass of September Update!

--- The Special Olympics Update/RUN THIS BEFORE UPDATING.bat
+++ The Special Olympics Update/RUN THIS BEFORE UPDATING.bat
@@ -6,4 +6,6 @@ del "base\characters\Kristoph\(c)glasses.apng"
 del "base\characters\Kristoph\(c)normal.apng"
 del "base\characters\Kristoph\(d)glasses.apng"
 rmdir "base\characters\Inferno_Cop\" /q /s
+rmdir "base\characters\Kazuma\" /q /s
+rmdir "base\characters\Beppo\" /q /s
 pause
