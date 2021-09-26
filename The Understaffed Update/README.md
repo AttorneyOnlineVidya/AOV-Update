148ddb8d9a1b71bd02a1f13eb0bfb2dcf6bb9513
AnUnknownQuantity <54252389+AnUnknownQuantity@users.noreply.github.com>
2021-09-26 13:29:40 -0500

    Preliminary pass of October Update!

---
+++ The Understaffed Update/README.md
@@ -0,0 +1,15 @@
+# [THE Unerstaffed UPDATE]
+
+Content additions:
+   * Added Phoenix PLvsAA
+   * Added Barry Caidin
+
+Content changes:
+   * Updated the anime.opus
+   * Reripped Hosonaga and Natsume
+   * Minor updates to Franzika AAI Young, Barok Wit, Crab, Ryunosuke, Flynch, Layton, Viridian, Birdly, Barnham and Judge GB
+   * Compressed the Inferno Cop rerip
+   * Renamed [PLMM] Objection! to [MM] Objection!, [PLvsAA] Mysterious Witness to [PLvsAA] Mysterious Fire, and [PLvsAA] Fiery People to [PLvsAA] Fiery Witnesses Enter the Court
+   * Added everyone else from CSS to evidence, alongside RNG
+   * Added looping for all AA, PLvsAA, and GT tracks, along with [DGS2] Trial (20th)
+   * Fixed the soundlist and courtroom sound inis
---
+++ The Understaffed Update/RUN THIS BEFORE UPDATING.bat
@@ -0,0 +1,14 @@
+del "base\sounds\music\[AOV] anime.opus"
+del "base\characters\Viridian\char_icon.apng"
+ren "base\sounds\music\[PLMM] Objection!.opus" "[MM] Objection!.opus" /q /s
+rmdir "base\characters\Soseki\" /q /s
+rmdir "base\characters\Satoru\" /q /s
+rmdir "base\characters\Inferno Cop\" /q /s
+rmdir "base\characters\Inferno_Cop_Disguised\" /q /s
+rmdir "base\characters\Inferno_Cop_Glitter\" /q /s
+rmdir "base\characters\Inferno_Cop_Punished\" /q /s
+rmdir "base\characters\Inferno_Cop_Unarmed\" /q /s
+rmdir "base\characters\Judge_GB\emotions" /q /s
+ren "base\sounds\music\[PLvsAA] Mysterious Witness.opus" "[PLvsAA] Mysterious Fire.opus" /q /s
+ren "base\sounds\music\[PLvsAA] Fiery People.opus" "[PLvsAA] Fiery Witnesses Enter the Court.opus" /q /s
+pause
