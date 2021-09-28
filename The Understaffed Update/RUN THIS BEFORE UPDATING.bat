8b22ce8209a711a63b785df8d595c8c5ee4e4a73
AnUnknownQuantity <54252389+AnUnknownQuantity@users.noreply.github.com>
2021-09-27 22:32:37 -0500

    (Hopefully) Final pass of October Update!

--- The Understaffed Update/RUN THIS BEFORE UPDATING.bat
+++ The Understaffed Update/RUN THIS BEFORE UPDATING.bat
@@ -1,6 +1,6 @@
 del "base\sounds\music\[AOV] anime.opus"
 del "base\characters\Viridian\char_icon.apng"
-ren "base\sounds\music\[PLMM] Objection!.opus" "base\sounds\music\[MM] Objection!.opus" /q /s
+ren "base\sounds\music\[PLMM] Objection!.opus" "[MM] Objection!.opus"
 rmdir "base\characters\Soseki\" /q /s
 rmdir "base\characters\Satoru\" /q /s
 rmdir "base\characters\Inferno_Cop\" /q /s
@@ -9,6 +9,7 @@ rmdir "base\characters\Inferno_Cop_Glitter\" /q /s
 rmdir "base\characters\Inferno_Cop_Punished\" /q /s
 rmdir "base\characters\Inferno_Cop_Unarmed\" /q /s
 rmdir "base\characters\Judge_GB\emotions" /q /s
-ren "base\sounds\music\[PLvsAA] Mysterious Witness.opus" "base\sounds\music\[PLvsAA] Mysterious Fire.opus" /q /s
-ren "base\sounds\music\[PLvsAA] Fiery People.opus" "base\sounds\music\[PLvsAA] Fiery Witnesses Enter the Court.opus" /q /s
+ren "base\sounds\music\[PLvsAA] Mysterious Witness.opus" "[PLvsAA] Mysterious Fire.opus"
+ren "base\sounds\music\[PLvsAA] Fiery People.opus" "[PLvsAA] Fiery Witnesses Enter the Court.opus"
+del "base\characters\Ryunosuke\custom.apng"
 pause
