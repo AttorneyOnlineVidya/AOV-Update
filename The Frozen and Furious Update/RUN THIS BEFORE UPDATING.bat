e175c18d77ad7a2d6a096a398f5b42ad20e9757d
Jumbowl <collinxchu@gmail.com>
2022-01-01 12:07:32 -0400

    First pass of january update.
    Adds all the content from the suggestion doc except for the theme edits.

---
+++ The Frozen and Furious Update/RUN THIS BEFORE UPDATING.bat
@@ -0,0 +1,18 @@
+del "base\sounds\music\[AOV] anime.opus"
+del "base\evidence\DDCaseCandice.png"
+del "base\evidence\AACastButz2.png"
+del "base\evidence\AACastButz3.png"
+del "base\evidence\AACastOldbag2.png"
+del "base\evidence\AACastPhoenix2.png"
+del "base\evidence\AACastPowers2.png"
+del "base\evidence\MiscBloodied_Bandges.png"
+del "base\evidence\AAI2Blanket.png"
+del "base\evidence\AAI2Chess.png"
+
+rmdir "base\background\AJCourtSideJ" /q /s
+rmdir "base\background\AJCourtSideJW" /q /s
+rmdir "base\background\AJCourtSideW" /q /s
+
+del /S /Q base\characters\Trucy_Red\*.gif
+
+pause
