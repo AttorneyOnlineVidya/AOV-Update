f7ef2d9a4f2fd20f9dd7cea7dec4f1365725d7df
AnUnknownQuantity <54252389+AnUnknownQuantity@users.noreply.github.com>
2023-02-07 16:24:06 -0600

    Secondary (late) pass to February Update!

--- The Singles Awareness Update/RUN THIS BEFORE UPDATING.bat
+++ The Singles Awareness Update/RUN THIS BEFORE UPDATING.bat
@@ -3,5 +3,5 @@ rmdir "base\characters\Judge_GB" /q /s
 rmdir "base\characters\Judge_JP" /q /s
 rmdir "base\characters\Altamont" /q /s
 rmdir "base\characters\Jigoku" /q /s
-ren "base\sounds\music\[P4U] Margaret's Theme - The Sadistic Stud-Keeping Secretary" "[P4U] Margaret - Sadistic Stud-Keeping Secretary"
+ren "base\sounds\music\[P4U] Margaret's Theme - The Sadistic Stud-Keeping Secretary.opus" "[P4U] Margaret - Sadistic Stud-Keeping Secretary.opus"
 pause
