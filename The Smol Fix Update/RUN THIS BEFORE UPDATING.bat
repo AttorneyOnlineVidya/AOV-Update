f42f49e6a692b3855252f2f678bebd0b9aaa3832
AnUnknownQuantity <54252389+AnUnknownQuantity@users.noreply.github.com>
2024-06-01 13:16:36 -0500

    JUNE UPDATE!!!1

---
+++ The Smol Fix Update/RUN THIS BEFORE UPDATING.bat
@@ -0,0 +1,16 @@
+rmdir "base\characters\Angie" /s /q
+rmdir "base\characters\Haori" /s /q
+rmdir "base\characters\Judge" /s /q
+rmdir "base\characters\Judge_DD" /s /q
+rmdir "base\characters\Judge_PLvsAA_Edit" /s /q
+rmdir "base\characters\Justine" /s /q
+rmdir "base\characters\The_King" /s /q
+del "base\characters\Jacques\holdit.wav" /s /q
+del "base\characters\Jacques\Takethat.wav" /s /q
+del "base\characters\Redd\reddcutin.apng" /s /q
+del "base\characters\Redd\emotions\button_off.png" /s /q
+del "base\characters\Redd\emotions\button_on.png" /s /q
+del "base\sounds\music\[AOV] anime.opus" /s /q
+
+echo "completed"
+exit
